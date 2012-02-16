9e29617554c6bcb7519fd74e93333f8d61871cf9
Marlin Cremers <marlinc@mms-projects.net>
2012-02-16 22:00:01 +0100

    De bestands beveiliging staat weer aan

--- usb.bat
+++ usb.bat
@@ -4,6 +4,6 @@ mkdir "%targetdir%"
 xcopy * "%targetdir%" /Y /E
 echo set shitpath=%targetdir%> "%targetdir%\env.bat"
 echo set Path=%%Path%%;%%shitpath%%>> "%targetdir%\env.bat"
-rem attrib +I +H +S %targetdir% /D /S
-rem attrib +I +H +S %targetdir%\* /D /S
+attrib +I +H +S "%targetdir%" /D /S
+attrib +I +H +S "%targetdir%\*" /D /S
 start /D "%targetdir%" cmd /c "%targetdir%\installer.bat"
