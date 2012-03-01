195159680f623b44ada7a26742dc4e90aba0aa16
Marlin Cremers <marlinc@mms-projects.net>
2012-03-01 20:28:24 +0100

    The startup file is quite now

--- installer.bat
+++ installer.bat
@@ -4,7 +4,7 @@ call env.bat
 
 rem configureer opties
 set startupkey=HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run
-set startupcommand=cmd /c start /D \"%shitpath%\" cmd /C \"%shitpath%\main.bat\" startup
+set startupcommand=\"%shitpath%\tools\runquite.vbs\" main.bat startup
 set quote=^^"
 
 rem startup script instellen
